4dcee1a08d4f0cd579842698893e04f0b6212902
lucas_cabrera1 <lucas_cabrera1@hotmail.com>
2022-08-13 13:15:52 -0300

    reescribo entrenamientos como componente funcional

--- entreno/frontend/src/App.js
+++ entreno/frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Ejercicios from './components/ejercicios/ejercicios2';
-import Entrenamientos from './components/entrenamientos/entrenamientos';
+import Entrenamientos from './components/entrenamientos/entrenamientos2';
 import NavBar from './components/navbar/navbar';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
@@ -17,6 +17,8 @@ function App() {
         <Routes>
           <Route path='/' element={<h2>Bienvenido</h2>}></Route>
           <Route path="/ejercicios" element={<Ejercicios />}></Route>
+          <Route path="/ejercicios/nuevo" element={<Ejercicios />}></Route>
+          <Route path="/ejercicios/editar/:id" element={<Ejercicios />}></Route>
           <Route path="/entrenamientos" element={<Entrenamientos />}></Route>
           <Route path='*' element={<h2>No se encontró la página</h2>}></Route>
         </Routes>
